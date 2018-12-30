a59ac47baa365fc27e3f2686d444d5b8e80d619d
argraur <argraur@gmail.com>
2018-12-30 13:08:42 +0000

    [SQUASH] polaris: import configs from stock and b1c1
    * Imported stock values from MIUI
    * Telephony and Telecom overlays
    * SettingsProvider
    * SystemUI
    
    Signed-off-by: argraur <argraur@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
